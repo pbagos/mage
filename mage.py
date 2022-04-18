4d2e17b9abb611ddebd0de25dd2308b27d28599f
gmanios <39456816+gmanios@users.noreply.github.com>
2022-04-18 21:21:51 +0300

    Update mage.py

--- mage.py
+++ mage.py
@@ -15,7 +15,7 @@ global settings, gprofiler_settings, version, studies
 settings = {}
 studies = []
 studies_transform = []
-version = '1.0.2'
+version = '1.0.3'
 
 
 def parse_args():
@@ -43,7 +43,7 @@ def parse_conf(conf_filename):
 if __name__ == '__main__':
     t0 = time.time()
     print("MAGE :: Meta-Analysis of Gene Expression")
-    print("Version " + version + "; September 2021")
+    print("Version " + version + "; March 2022")
     print("Copyright (C) 2021 Pantelis Bagos")
     print("Freely distributed under the GNU General Public Licence (GPLv3)")
     print("--------------------------------------------------------------------------")
@@ -52,9 +52,9 @@ if __name__ == '__main__':
     parse_conf(args.c)
     filepath = args.o
 
-    print("Load File data")
+    print("Loading File data")
     file_list = list(settings['study_files'].split(","))
-
+    alpha = float(settings['significance_level'])
     for i in range(len(file_list)):
         # Read file data
         studypath = settings['study_dir'] + file_list[i].strip()
@@ -77,23 +77,23 @@ if __name__ == '__main__':
         metanalysis_df = multivariate.run(settings, data, filepath)
         metanalysis_df.to_csv(filepath + 'multivariate_analysis_results.txt', sep='\t', mode='w')
     else:
-        print('Meta Analysis started')
+        print('Meta-analysis started')
         metanalysis_df = meta_analysis.run(settings, data)
         metanalysis_df.to_csv(filepath + 'meta_analysis_results.txt', sep='\t', mode='w')
 
         # create and save plots
         if settings.get('plots') == 'YES':
-            plots.meta_analysis_plots(metanalysis_df, filepath)
+            plots.meta_analysis_plots(metanalysis_df, filepath,alpha)
 
     if settings.get('enrichment_analysis') == 'YES':
         print('Enrichment Analysis started')
-        alpha = float(settings['alpha'])
-        genes_for_ea = metanalysis_df['Genes'].where(
-            np.array(metanalysis_df['p_value'], dtype=float) < alpha).dropna().tolist()
 
+        genes_for_ea = metanalysis_df['genes_step_up'].where(
+            np.array(metanalysis_df['p_values_step_up'], dtype=float) < np.array(metanalysis_df['simes'], dtype=float) ).dropna().tolist()
+        print(str(len(genes_for_ea))+' genes for Enrichment Analysis')
         enrichment_analysis_df = enrichment_analysis.run(settings, genes_for_ea)
         enrichment_analysis_df.to_csv(filepath + 'enrichment_analysis_results.txt',
-                                      header=enrichment_analysis_df.columns, index=None, sep='\t', mode='a')
+                                      header=enrichment_analysis_df.columns, index=None, sep='\t', mode='w')
         if settings.get('plots') == 'YES':
             plots.ea_manhattan_plot(enrichment_analysis_df, filepath, settings['threshold'])
             plots.ea_heatmap_plot(enrichment_analysis_df, filepath)
